b8f95f82dc6e299e21c4cfc8c1a5aaf88e545794
下山化缘的小和尚 <work.cjl@hotmail.com>
2021-11-15 14:58:24 +0800

    feat: 给devserver添加onAfterSetupMiddleware使其变成mock服务器

--- webpack.config.js
+++ webpack.config.js
@@ -35,19 +35,37 @@ module.exports = {
       publicPath: '/abc/' // 告诉服务器在哪个 URL 上提供 static.directory 的内容
     },
     port: 8000,
-    proxy: {
-      /**
-       * 工作流程：webpack devserver接到浏览器发来的http://localhost:8000/api/users的请求
-       * webpack devserver获取/api/users路径，并将其重写，将/api替换成空，最终的重写路径是/users
-       * 然后webpack devserver将这个路径发送给3000应用服务器，最终的请求是http://localhost:3000/users
-       * 3000应用服务器发送给webpack devserver响应的数据之后，webpack devserver再将其发送回给浏览器
-       */
-      '/api': {
-        target: 'http://localhost:3000',
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
+    // proxy: {
+    //   /**
+    //    * 工作流程：webpack devserver接到浏览器发来的http://localhost:8000/api/users的请求
+    //    * webpack devserver获取/api/users路径，并将其重写，将/api替换成空，最终的重写路径是/users
+    //    * 然后webpack devserver将这个路径发送给3000应用服务器，最终的请求是http://localhost:3000/users
+    //    * 3000应用服务器发送给webpack devserver响应的数据之后，webpack devserver再将其发送回给浏览器
+    //    */
+    //   '/api': {
+    //     target: 'http://localhost:3000',
+    //     pathRewrite: {
+    //       '^/api': ''
+    //     }
+    //   }
+    // },
+    /**
+     * 使用onAfterSetupMiddleware可以将devserver变成mock服务器
+     * 这样子方便在后端的应用服务器没开发完成的时候前端自己进行数据mock
+     */
+    onAfterSetupMiddleware: ({ app }) => {
+      app.get('/users', function (_req, res) {
+        res.json([
+          {
+            id: 1,
+            name: 'csy'
+          },
+          {
+            id: 2,
+            name: 'cjl'
+          }
+        ])
+      })
     }
   },
   module: {
